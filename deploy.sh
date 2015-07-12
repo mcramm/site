5f0e7dc7722a6d7d626fe8d4571caf107260707a
Michael Cramm <gmcramm@gmail.com>
2015-07-12 10:06:32 -0600

    Add submodule for content, update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project. 
 hugo
 
+cd public
+
 # Add changes to git.
 git add -A
 
@@ -15,6 +17,6 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:mcramm/mcramm.github.com.git gh-pages
+
+cd ..
